24fb860c4240f25ae96b32b7ac0cf0d3d7dd769d
salpreh <salva.perez46@gmail.com>
2024-08-16 12:22:38 +0200

    Init repository

---
+++ src/main/java/com/salpreh/products/ModulithProductsApplication.java
@@ -0,0 +1,13 @@
+package com.salpreh.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulithProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModulithProductsApplication.class, args);
+	}
+
+}
