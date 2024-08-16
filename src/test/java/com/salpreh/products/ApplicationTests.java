48f5f3074ab29e3aaf6c77161106128641e1f1fa
salpreh <salva.perez46@gmail.com>
2024-08-16 23:11:24 +0200

    test: Modulith validation and doc generation

--- src/test/java/com/salpreh/products/ApplicationTests.java
+++ src/test/java/com/salpreh/products/ApplicationTests.java
@@ -1,13 +1,34 @@
 package com.salpreh.products;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 @SpringBootTest
 class ApplicationTests {
 
+  private ApplicationModules modules = ApplicationModules.of(Application.class);
+
 	@Test
 	void contextLoads() {
 	}
+
+
+  @Test
+  void validModuleStructure() {
+    modules.verify();
+  }
+
+  @Test
+  void validDocumentation() {
+    assertDoesNotThrow(() -> {
+      new Documenter(modules)
+        .writeModulesAsPlantUml()
+        .writeIndividualModulesAsPlantUml();
+    });
+  }
 
 }
