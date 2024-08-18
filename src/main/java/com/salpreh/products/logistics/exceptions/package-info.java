2636cdf76e73884b10d53f6161bd593164833787
salpreh <salva.perez46@gmail.com>
2024-08-18 23:42:12 +0200

    feat: Expose exception packages

---
+++ src/main/java/com/salpreh/products/logistics/exceptions/package-info.java
@@ -0,0 +1,4 @@
+@NamedInterface("models")
+package com.salpreh.products.logistics.exceptions;
+
+import org.springframework.modulith.NamedInterface;
---
+++ src/main/java/com/salpreh/products/products/exceptions/package-info.java
@@ -0,0 +1,4 @@
+@NamedInterface("models")
+package com.salpreh.products.products.exceptions;
+
+import org.springframework.modulith.NamedInterface;
