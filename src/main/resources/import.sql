1ae6b57e6ba1228722d9929869a1377decca1738
salpreh <salva.perez46@gmail.com>
2024-08-18 23:33:36 +0200

    feat: Updated import sql script

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,12 +1,12 @@
 -- Products inserts
-INSERT INTO products(purchase_price, selling_price, barcode, description, image_url, name) VALUES(5.0, 6.5, '000001', 'Arrocete', 'http://image.com', 'Arroz de Valencia');
-INSERT INTO products(purchase_price, selling_price, barcode, description, image_url, name) VALUES(2.2, 2.6, '000002', 'Galletas', 'http://image.com', 'Galletas sin gluten');
-INSERT INTO products(purchase_price, selling_price, barcode, description, image_url, name) VALUES(1.0, 1.5, '000003', 'Leche', 'http://image.com', 'Leche de vaca');
+INSERT INTO products(purchase_price, selling_price, barcode, description, image_url, name) VALUES(5.0, 6.5, '00000000000001', 'Arrocete', 'http://image.com', 'Arroz de Valencia');
+INSERT INTO products(purchase_price, selling_price, barcode, description, image_url, name) VALUES(2.2, 2.6, '00000000000002', 'Galletas', 'http://image.com', 'Galletas sin gluten');
+INSERT INTO products(purchase_price, selling_price, barcode, description, image_url, name) VALUES(1.0, 1.5, '00000000000003', 'Leche', 'http://image.com', 'Leche de vaca');
 
-INSERT INTO products_tags(product_entity_barcode, tags) VALUES('000001', 'arroz');
-INSERT INTO products_tags(product_entity_barcode, tags) VALUES('000002', 'reposteria');
-INSERT INTO products_tags(product_entity_barcode, tags) VALUES('000002', 'sin gluten');
-INSERT INTO products_tags(product_entity_barcode, tags) VALUES('000003', 'lacteos');
+INSERT INTO products_tags(product_entity_barcode, tags) VALUES('00000000000001', 'arroz');
+INSERT INTO products_tags(product_entity_barcode, tags) VALUES('00000000000002', 'reposteria');
+INSERT INTO products_tags(product_entity_barcode, tags) VALUES('00000000000002', 'sin gluten');
+INSERT INTO products_tags(product_entity_barcode, tags) VALUES('00000000000003', 'lacteos');
 
 INSERT INTO addresses(id, city, country_code, street, zip_code) VALUES(1, 'Valencia', 'ES', 'Calle de la Paz, 123', '46001');
 INSERT INTO addresses(id, city, country_code, street, zip_code) VALUES(2, 'Valencia', 'ES', 'Carrer Andres Estelles, 34', '46002');
@@ -17,17 +17,17 @@ INSERT INTO suppliers(billing_address_id, id, iban, name) VALUES(2, 2, 'ES662100
 INSERT INTO suppliers(billing_address_id, id, iban, name) VALUES(3, 3, 'ES6621000418401234567893', 'Leche y Repostería S.A.');
 ALTER SEQUENCE address_id_seq RESTART WITH 4;
 
-INSERT INTO products_suppliers(supplier_id, product_barcode) VALUES (1, '000001');
-INSERT INTO products_suppliers(supplier_id, product_barcode) VALUES (2, '000001');
-INSERT INTO products_suppliers(supplier_id, product_barcode) VALUES (3, '000002');
-INSERT INTO products_suppliers(supplier_id, product_barcode) VALUES (3, '000003');
+INSERT INTO products_suppliers(supplier_id, product_barcode) VALUES (1, '00000000000001');
+INSERT INTO products_suppliers(supplier_id, product_barcode) VALUES (2, '00000000000001');
+INSERT INTO products_suppliers(supplier_id, product_barcode) VALUES (3, '00000000000002');
+INSERT INTO products_suppliers(supplier_id, product_barcode) VALUES (3, '00000000000003');
 
 -- Stores inserts
 INSERT INTO stores(code, name) VALUES (3000, 'Store 1');
 INSERT INTO stores(code, name) VALUES (3001, 'Store 2');
 INSERT INTO stores(code, name) VALUES (3002, 'Store 3');
 
-INSERT INTO store_stock(quantity, store_code, product_barcode) VALUES (50, 3000, '000001');
-INSERT INTO store_stock(quantity, store_code, product_barcode) VALUES (60, 3000, '000002');
-INSERT INTO store_stock(quantity, store_code, product_barcode) VALUES (70, 3000, '000003');
-INSERT INTO store_stock(quantity, store_code, product_barcode) VALUES (80, 3001, '000001');
+INSERT INTO store_stock(quantity, store_code, product_barcode) VALUES (50, 3000, '00000000000001');
+INSERT INTO store_stock(quantity, store_code, product_barcode) VALUES (60, 3000, '00000000000002');
+INSERT INTO store_stock(quantity, store_code, product_barcode) VALUES (70, 3000, '00000000000003');
+INSERT INTO store_stock(quantity, store_code, product_barcode) VALUES (80, 3001, '00000000000001');
