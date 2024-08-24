6e8acd67143b9f9faebea1744682cd71e785fbc2
salpreh <salva.perez46@gmail.com>
2024-08-25 01:37:45 +0200

    feat: Create pallet operation

--- src/main/java/com/salpreh/products/restapi/controllers/PalletController.java
+++ src/main/java/com/salpreh/products/restapi/controllers/PalletController.java
@@ -5,6 +5,7 @@ import com.salpreh.products.logistics.models.Pallet;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +17,13 @@ public class PalletController {
 
   private final PalletUseCasePort palletUseCase;
 
-  @GetMapping
+  @GetMapping("/decode")
   public ResponseEntity<Pallet> decodeEan128(@RequestParam String eanCode) {
     return ResponseEntity.ok(palletUseCase.decodeEan128(eanCode));
+  }
+
+  @PostMapping
+  public ResponseEntity<Pallet> createPallet(@RequestParam String eanCode) {
+    return ResponseEntity.ok(palletUseCase.createPallet(eanCode));
   }
 }
